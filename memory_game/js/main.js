7d7d6772236462d45bcf19e87cfbc7a7bc06a4a1
Cookie <tsolbrig87@gmail.com>
2019-10-07 14:55:17 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,15 @@
-console.log("Up and running!")
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-console.log("User Flipped " + cardOne);
-console.log("User Flipped " + cardFour);
+const cards = ["queen","queen","king","king"];
+const cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardsInPlay[0]);
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardsInPlay[1]);
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
